c42d9f5d18a18f0fdf89a9ac1ce4f7374fab0445
nuengbenjamas <79198914+nuengbenjamas@users.noreply.github.com>
2023-11-09 09:51:49 +0700

    Update content_layout.dart

--- lib/content_layout.dart
+++ lib/content_layout.dart
@@ -119,3 +119,61 @@ class ContentLayout extends StatelessWidget {
     );
   }
 }
+
+class Main extends StatefulWidget {
+  const Main({super.key});
+
+  @override
+  State<Main> createState() => _MainState();
+}
+
+class _MainState extends State<Main> {
+  final TextEditingController _textController = TextEditingController(text: '');
+  String data = '';
+  final GlobalKey _qrkey = GlobalKey();
+ 
+  Future getUserData() async {
+    var response = await http.get(Uri.parse('https://partner1.triggersplus.com/dining/qr_code_test/APP_SECRET_KEY/'));
+    var jsonData = jsonDecode(response.body);
+
+
+    List<User> users = []; 
+    return jsonData; 
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('User Data'),
+      ),
+      body: Container(
+        child: Card(
+          child: FutureBuilder(
+              future: getUserData(),
+              builder: (context, snapshot) {
+               // print("###1 ${snapshot.data}");
+               // print("###2 ${snapshot.data['status']}");
+               // print("###3 ${snapshot.data['status']['code']}");
+               // print("###4 ${snapshot.data['status']['description']}");
+               // print("###5 ${snapshot.data['data']}");
+                print("###6 ${snapshot.data['data']['qrRawData']}"); 
+                return Container(
+                    child: Center(
+                      child: Text(snapshot.data['data']['qrRawData']),
+                    ),
+                  );
+              }),
+        ),
+      ),
+    );
+  }
+}
+
+
+class User {
+  final String status, data;
+  User(this.status, this.data,);
+}
